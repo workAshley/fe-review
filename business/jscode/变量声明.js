c817e43ebaa14618ea3ebcb79c9fed9be540c5ab
Ashley <job.csy@hotmail.com>
2022-04-03 09:56:22 +0800

    feat: 变量提升

--- business/jscode/变量声明.js
+++ business/jscode/变量声明.js
@@ -1,5 +1,5 @@
 c()
-d()
+// d()
 function c() {
   let a = 1
   console.log(a)
@@ -9,5 +9,17 @@ var d = () => {
   let b = 2
   console.log(b)
 }
-// d is not a function  
+// d is not a function
 // 箭头函数不能发生函数提升
+console.log(e) // undefined
+var e = 4
+console.log(e) // 4
+// console.log(f);   // f is not defined
+// console.log(g)     // g is not defined
+function foo() {
+  let f = 5
+  var g = 6
+}
+// console.log(f);  // f is not defined
+console.log(g)  // g is not defined
+
